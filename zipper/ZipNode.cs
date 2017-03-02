ef49e9ab31457de4597a8e9ccd1eba6417f3a430
Michael Brehm <djp952@gmail.com>
2017-03-01 23:33:04 -0500

    zipper v1.0.0
    zipper is a command-line utility that creates a zip archive file based
    on an input XML manifest.  Useful as part of builds that require a zip
    file as an output in lieu of something more robust like 7-zip

---
+++ zipper/ZipNode.cs
@@ -0,0 +1,54 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) 2017 Michael G. Brehm
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//-----------------------------------------------------------------------------
+
+using System;
+
+namespace zuki.build.tools
+{
+	/// <summary>
+	/// Holds node information loaded from the manifest.xml file
+	/// </summary>
+	class ZipNode
+	{
+		/// <summary>
+		/// Instance Constructor
+		/// </summary>
+		public ZipNode()
+		{
+		}
+
+		/// <summary>
+		/// Path to the source file if applicable
+		/// </summary>
+		public string Source = String.Empty;
+
+		/// <summary>
+		/// Destination path within the generated archive
+		/// </summary>
+		public string Path = String.Empty;
+
+		/// <summary>
+		/// Flag to normalize file node line endings before output
+		/// </summary>
+		public ZipNormalizeType Normalize = ZipNormalizeType.None;
+	}
+}
---
+++ zipper/main.cs
@@ -0,0 +1,296 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) 2017 Michael G. Brehm
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//-----------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------
+// Usage: 
+//
+// > zipper create {zipfile} {manifest}
+//
+// Creates a new ZIP archive based on an archive manifest
+//
+//	zipfile		- zip archive file to be created
+//	manifest	- manifest file describing the archive contents
+//---------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace zuki.build.tools
+{
+	class main
+	{
+		/// <summary>
+		/// Application entry point
+		/// </summary>
+		/// <param name="arguments">Command-line arguments</param>
+		static int Main(string[] arguments)
+		{
+			try
+			{
+				// Parse the command line arguments and switches
+				CommandLine commandline = new CommandLine(arguments);
+
+				// If insufficient arguments specified or help was requested, display the usage and exit
+				if ((commandline.Arguments.Count == 0) || (commandline.Switches.ContainsKey("?")))
+				{
+					ShowUsage();
+					return 0;
+				}
+
+				// Get the application command
+				string command = commandline.Arguments[0].ToLower();
+
+				// > create {zipfile} {manifest}
+				if (command == "create")
+				{
+					if (commandline.Arguments.Count != 3) { ShowUsage(); return 1; }
+					CreateZip(commandline.Arguments[1], commandline.Arguments[2]);
+				}
+
+				// unrecognized command
+				else { ShowUsage(); }
+
+				return 0;
+			}
+
+			catch (Exception ex)
+			{
+				// Nothing fancy, just catch anything that escapes and spit it out
+				Console.WriteLine();
+				Console.WriteLine("ERROR: " + ex.Message);
+				Console.WriteLine();
+		
+				return unchecked((int)0x80004005);		// <-- E_FAIL
+			}
+		}
+
+		//-------------------------------------------------------------------
+		// Private Member Functions
+		//-------------------------------------------------------------------
+
+		/// <summary>
+		/// Creates a new zip file based on the contents of an input manifest file
+		/// </summary>
+		/// <param name="zipfile">zip file to be created</param>
+		/// <param name="manifestfile">zip manifest file name</param>
+		private static void CreateZip(string zipfile, string manifestfile)
+		{
+			// Attempt to load the contents of the manifest file into a List<>
+			List<ZipNode> nodes = LoadManifest(manifestfile);
+
+			// Append the zip file name to the current directory in case it's relative
+			zipfile = Path.Combine(Environment.CurrentDirectory, zipfile);
+
+			// Ensure that the specified output directory exists
+			if (!Directory.Exists(Path.GetDirectoryName(zipfile)) && !TryCreateDirectory(Path.GetDirectoryName(zipfile)))
+				throw new DirectoryNotFoundException("specified output directory [" + Path.GetDirectoryName(zipfile) + "] does not exist");
+
+			// Attempt to delete any existing file at the specified location; ZipFile.Open will fail with "exists" if this doesn't work
+			if (File.Exists(zipfile)) TryDeleteFile(zipfile);
+
+			try
+			{
+                // Spit out the name of the archive file being created
+                Console.WriteLine();
+				Console.WriteLine("create: " + zipfile);
+
+				// Create the output archive file stream
+				using (ZipArchive archive = ZipFile.Open(zipfile, ZipArchiveMode.Create))
+				{
+					foreach (ZipNode node in nodes)
+					{
+						string sourcefile = node.Source;        // Initial source file path
+						bool deletesource = false;              // Flag to delete source file after processing
+
+						// If creating a file node with the NormalizeLineEndings flag set, generate a normalized temporary 
+						// file that will take it's place in the archive
+						if ((node.Normalize != ZipNormalizeType.None) && (!String.IsNullOrEmpty(node.Source)))
+						{
+							sourcefile = NormalizeFile(node.Source, node.Normalize);
+							deletesource = true;
+						}
+
+						// Write the file into the archive and delete any temporary source that was generated
+						try { archive.CreateEntryFromFile(sourcefile, node.Path, CompressionLevel.Optimal); }
+						finally { if (deletesource) TryDeleteFile(sourcefile); }
+
+						// Just spit out the resultant file name as an "add" operation for now
+						Console.WriteLine("add: " + node.Path);
+					}
+				}
+			}
+
+			// Delete the created zip file on any exception during creation
+			catch { TryDeleteFile(zipfile); throw; }
+		}
+
+		/// <summary>
+		/// Loads all of the zip file information from the manifest file
+		/// </summary>
+		/// <param name="manifestfile">Path to the manifest.xml file</param>
+		private static List<ZipNode> LoadManifest(string manifestfile)
+		{
+			// Ensure that the manifest file exists
+			if (!File.Exists(manifestfile)) throw new FileNotFoundException("specified manifest file [" + manifestfile + "] does not exist", manifestfile);
+
+			// Create a new empty list of nodes
+			List<ZipNode> zipnodes = new List<ZipNode>();
+
+			// Create a new XmlDocument instance for the specified input file path
+			XmlDocument document = new XmlDocument();
+			document.Load(manifestfile);
+
+			// Process each child element under the document element
+			foreach (XmlNode child in document.DocumentElement.ChildNodes)
+			{
+				if (child.NodeType != XmlNodeType.Element) continue;
+				XmlElement element = (XmlElement)child;
+
+				// Construct the ZipNode element to be populated
+				ZipNode node = new ZipNode();
+
+				try
+				{
+					// Get the name of the child element (<file>)
+					string elementname = child.Name.ToLower();
+
+					// <file>
+					if (elementname == "file")
+					{
+						// Required: source= 
+						node.Source = element.GetAttribute("source");
+						if (String.IsNullOrEmpty(node.Source)) throw new Exception("missing or empty attribute 'source'");
+
+						// Required: path=
+						node.Path = element.GetAttribute("path");
+						if (String.IsNullOrEmpty(node.Path)) throw new Exception("missing or empty attribute 'path'");
+
+						// Optional: normalize= [default: none]
+						string normalize = element.GetAttribute("normalize");
+						if(!String.IsNullOrEmpty(normalize) && !Enum.TryParse<ZipNormalizeType>(normalize, true, out node.Normalize)) throw new Exception("unable to parse attribute 'normalize'");
+					}
+
+					else throw new Exception("unexpected element <" + elementname + ">");
+
+					// Add the ZipNode instance into the collection
+					zipnodes.Add(node);
+				}
+
+				// Reformat the exception to include some context, the standard System.Xml.XmlDocument object
+				// does not maintain line/column numbers so dump the first 40 characters of the line
+				catch (Exception ex) { throw new Exception(String.Format("Manifest element [{0}...]: {1}", element.OuterXml.Substring(0, 40), ex.Message)); }
+			}
+
+			return zipnodes;
+		}
+
+		/// <summary>
+		/// Normalizes the line endings of a text file
+		/// </summary>
+		/// <param name="filename">Input file name</param>
+		/// <param name="type">Normalization type</param>
+		private static string NormalizeFile(string filename, ZipNormalizeType type)
+		{
+			if (!File.Exists(filename)) throw new FileNotFoundException("specified text file [" + filename + "] does not exist", filename);
+			if (type == ZipNormalizeType.None) throw new ArgumentException("type");
+
+			// Use a system-generated temporary file as the target
+			string outfile = Path.GetTempFileName();
+
+			try
+			{
+				// Open up a reader against the source file ...
+				using (StreamReader sr = new StreamReader(File.OpenRead(filename)))
+				{
+					// ... and a writer against the destination file
+					using (StreamWriter sw = new StreamWriter(File.Create(outfile)))
+					{
+						// Select either Windows or Unix line endings to apply to the file
+						sw.NewLine = (type == ZipNormalizeType.Unix) ? "\n" : "\r\n";
+
+						// Just read in every line from the source file and write it back
+						// out to the destination file using the defined line ending
+						string line = sr.ReadLine();
+						while (line != null)
+						{
+							sw.WriteLine(line);
+							line = sr.ReadLine();
+						}
+
+						// Flush the output stream to disk
+						sw.Flush();
+					}
+				}
+
+				// Return the name of the normalized file back to the caller
+				return outfile;
+			}
+
+			// Delete the temporary file on any exception during conversion
+			catch { TryDeleteFile(outfile); throw; }
+		}
+
+		/// <summary>
+		/// Shows application usage information
+		/// </summary>
+		private static void ShowUsage()
+		{
+			Console.WriteLine();
+			Console.WriteLine("zipper - zip archive creation tool");
+			Console.WriteLine();
+			Console.WriteLine("Usage:");
+			Console.WriteLine();
+			Console.WriteLine("> zipper create {zipfile} {manifest}");
+			Console.WriteLine();
+			Console.WriteLine("Creates a new ZIP archive based on an archive manifest");
+			Console.WriteLine();
+			Console.WriteLine("    zipfile   - zip archive file to be created");
+			Console.WriteLine("    manifest  - manifest file describing the archive contents");
+			Console.WriteLine();
+		}
+
+		/// <summary>
+		/// Attempts to create a directory; does not throw any exceptions
+		/// </summary>
+		/// <param name="path">Path to the file to be deleted</param>
+		private static bool TryCreateDirectory(string path)
+		{
+			try { Directory.CreateDirectory(path); return true; }
+			catch { return false; }
+		}
+
+		/// <summary>
+		/// Attempts to delete a file; does not throw any exceptions
+		/// </summary>
+		/// <param name="path">Path to the file to be deleted</param>
+		private static bool TryDeleteFile(string path)
+		{
+			try { File.Delete(path); return true; }
+			catch { return false; }
+		}
+	}
+}
